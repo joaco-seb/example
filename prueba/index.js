42ab54a40b534bd8ecb9c11b5a3847d49b142f61
JoaquinHubner <joaquinhubner42@gmail.com>
2025-03-01 01:56:46 -0300

    sexto commit

--- prueba/index.js
+++ prueba/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 
 let notes = [
   {
@@ -20,9 +19,26 @@ let notes = [
   }
 ]
 
-app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const cors = require('cors')
+
+app.use(cors())
+
+app.use(express.json())
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -76,6 +92,8 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end()
 })
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
